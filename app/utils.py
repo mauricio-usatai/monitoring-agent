f4cd160fbe8ce41fc65d5e204b7ce9bc15be5fd1
mauricio.usatai@gmail.com <mauricio.usatai@gmail.com>
2022-11-24 08:33:34 -0300

    timedelta test

--- app/utils.py
+++ app/utils.py
@@ -4,6 +4,8 @@ def get_time_delta(frequency, date):
   date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S.%f')
   parsed_frequency = frequency.split(' ')
   if parsed_frequency[2] == 'hours':
+    print(int(parsed_frequency[1]) * 3600 - (datetime.now() - date).seconds)
     return int(parsed_frequency[1]) * 3600 - (datetime.now() - date).seconds
   elif parsed_frequency[2] == 'minutes':
+    print(int(parsed_frequency[1]) * 60 - (datetime.now() - date).seconds)
     return int(parsed_frequency[1]) * 60 - (datetime.now() - date).seconds
